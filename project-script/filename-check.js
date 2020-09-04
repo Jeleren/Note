14081e9b2fd9265c0b219f04a14d1d5a0768dba5
Jeleren <17771396576@163.com>
2020-09-04 18:25:49 +0800

    fix: 添加文件名检测

--- project-script/filename-check.js
+++ project-script/filename-check.js
@@ -3,5 +3,11 @@
  * */
 
 const { execSync } = require("child_process")
-const res = execSync("git diff --staged --name-only").toString()
-console.log(res)
+const res = execSync("git diff --staged --name-only").toString().trim()
+// 文件中包含大写字母的不符合规范
+// 两个及以上连续的短横线不符合规范
+const test = res.split(/\//).filter((v) => v.match(/[^-][A-Z]+[^-]/) || v.match(/-{2,}/))
+if(test.length > 0) {
+    console.log('以下文件命名不规范')
+    test.forEach(i => console.log(i))
+}
